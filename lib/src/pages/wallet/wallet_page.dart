a63039d7952ca87133aa6a0dfc8f47d176d6dbf8
2DT <david@iop-ventures.com>
2021-03-26 13:54:00 +0100

    Added centered text if wallet is empty

--- lib/src/pages/wallet/wallet_page.dart
+++ lib/src/pages/wallet/wallet_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iop_wallet/src/models/wallet/wallet.dart';
+import 'package:iop_wallet/src/theme.dart';
 import 'package:provider/provider.dart';
 
 import 'credential_details.dart';
@@ -28,31 +29,37 @@ class _CredentialList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wallet = context.watch<WalletModel>();
-    return ListView.builder(
-      itemCount: wallet.credentials.length,
-      itemBuilder: (context, index) => ListTile(
-        leading: Icon(Icons.card_membership,
-            color: Theme.of(context).primaryColorDark),
-        title: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        CredentialRoute(wallet.credentials[index])));
-          },
-          child: Text(wallet.credentials[index].credentialName),
-        ),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.remove_circle_outline,
-            color: Colors.red,
-          ),
-          onPressed: () {
-            wallet.remove(wallet.credentials[index]);
-          },
-        ),
-      ),
-    );
+    return (wallet.credentials.isEmpty)
+        ? Center(
+            child: Text(
+            'You have no credentials in your wallet.',
+            style: textTheme.headline2,
+          ))
+        : ListView.builder(
+            itemCount: wallet.credentials.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.card_membership,
+                  color: Theme.of(context).primaryColorDark),
+              title: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              CredentialRoute(wallet.credentials[index])));
+                },
+                child: Text(wallet.credentials[index].credentialName),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.remove_circle_outline,
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  wallet.remove(wallet.credentials[index]);
+                },
+              ),
+            ),
+          );
   }
 }
