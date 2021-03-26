54fcd76911f6702cdbd9182e34b8538c15f7c34e
2DT <david@iop-ventures.com>
2021-03-26 13:17:20 +0100

    Added createMnemonicTabel utility functions removed by merge

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 final double buttonWidth = 100;
@@ -13,4 +14,34 @@ Future<String> scanQrUntilResult() async {
   final barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
       '#ff6666', 'Cancel', true, ScanMode.QR);
   return barcodeScanRes;
+}
+
+List<TableRow> createMnemonicTable(List<String> mnemonicList) {
+  final mnemonicTable = <TableRow>[];
+  var tableRow = <Widget>[];
+
+  for (var i = 0; i < mnemonicList.length; i++) {
+    Widget tile = createListTile(i, mnemonicList[i]);
+    tableRow.add(tile);
+
+    if (tableRow.length == 4) {
+      mnemonicTable.add(TableRow(children: tableRow));
+      tableRow = [];
+    }
+  }
+  return mnemonicTable;
+}
+
+ListTile createListTile(int index, String content) {
+  final style = TextStyle(fontSize: 12, color: Colors.white);
+  return ListTile(
+      dense: true,
+      minLeadingWidth: 1,
+      horizontalTitleGap: 4,
+      contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+      leading: Text(
+        '${index + 1}.',
+        style: style,
+      ),
+      title: Text(content, style: style));
 }
